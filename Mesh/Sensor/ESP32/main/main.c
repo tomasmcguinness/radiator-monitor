b8cd8c979d1d9d4c22712fa165c7a52561ac120b
Tomas McGuinness <tomas@tomasmcguinness.com>
2023-11-14 09:43:45 +0000

    Auto enter network provisioning now appears to complete

--- Mesh/Sensor/ESP32/main/main.c
+++ Mesh/Sensor/ESP32/main/main.c
@@ -11,10 +11,11 @@
 #include "freertos/task.h"
 #include "esp_flash.h"
 #include "nvs_flash.h"
-
+#include <string.h>
 #include "mesh.h"
 #include "mesh_main.h"
-
+#include "crypto.h"
+#include "access.h"
 #include "esp_ble_mesh_common_api.h"
 #include "esp_ble_mesh_provisioning_api.h"
 #include "esp_ble_mesh_networking_api.h"
@@ -22,10 +23,12 @@
 #include "esp_ble_mesh_generic_model_api.h"
 #include "esp_ble_mesh_sensor_model_api.h"
 
+#include "ble_mesh_example_init.h"
+#include "ble_mesh_example_nvs.h"
+
 #define TAG "SENSOR"
 
-// TODO THIS MUST BE DYNAMIC!!
-static uint8_t dev_uuid[16] = {0xdd, 0xdd};
+static nvs_handle_t NVS_HANDLE;
 
 /***************
  * BLUETOOTH MESH
@@ -34,19 +37,24 @@ static uint8_t dev_uuid[16] = {0xdd, 0xdd};
 
 static uint8_t dev_uuid[16] = {0xdd, 0xdd};
 
+struct bt_mesh_device_network_info
+{
+    uint8_t net_key[16];
+    uint16_t net_idx;
+    uint8_t flags;
+    uint32_t iv_index;
+    uint16_t unicast_addr;
+    uint8_t dev_key[16];
+    uint8_t app_key[16];
+    uint16_t app_idx;
+    uint16_t group_addr;
+};
+
 static esp_ble_mesh_cfg_srv_t config_server = {
     .relay = ESP_BLE_MESH_RELAY_DISABLED,
     .beacon = ESP_BLE_MESH_BEACON_ENABLED,
-#if defined(CONFIG_BLE_MESH_FRIEND)
-    .friend_state = ESP_BLE_MESH_FRIEND_ENABLED,
-#else
     .friend_state = ESP_BLE_MESH_FRIEND_NOT_SUPPORTED,
-#endif
-#if defined(CONFIG_BLE_MESH_GATT_PROXY_SERVER)
-    .gatt_proxy = ESP_BLE_MESH_GATT_PROXY_ENABLED,
-#else
     .gatt_proxy = ESP_BLE_MESH_GATT_PROXY_NOT_SUPPORTED,
-#endif
     .default_ttl = 7,
     /* 3 transmissions with 20ms interval */
     .net_transmit = ESP_BLE_MESH_TRANSMIT(2, 20),
@@ -56,11 +64,11 @@ static esp_ble_mesh_cfg_srv_t config_server = {
 #define ESP_BLE_MESH_VND_COMPANY_ID 0x0059
 #define ESP_BLE_MESH_VND_MODEL_ID 0x000A
 
-#define ESP_BLE_MESH_VND_MODEL_OP_SET ESP_BLE_MESH_MODEL_OP_3(0x0A, ESP_BLE_MESH_VND_COMPANY_ID)
-#define ESP_BLE_MESH_VND_MODEL_OP_SET_STATUS ESP_BLE_MESH_MODEL_OP_3(0x0B, ESP_BLE_MESH_VND_COMPANY_ID)
+#define ESP_BLE_MESH_VND_MODEL_OP_READING_SET ESP_BLE_MESH_MODEL_OP_3(0x0A, ESP_BLE_MESH_VND_COMPANY_ID)
+#define ESP_BLE_MESH_VND_MODEL_OP_READING_SET_STATUS ESP_BLE_MESH_MODEL_OP_3(0x0B, ESP_BLE_MESH_VND_COMPANY_ID)
 
 static const esp_ble_mesh_client_op_pair_t vnd_op_pair[] = {
-    {ESP_BLE_MESH_VND_MODEL_OP_SET, ESP_BLE_MESH_VND_MODEL_OP_SET_STATUS},
+    {ESP_BLE_MESH_VND_MODEL_OP_READING_SET, ESP_BLE_MESH_VND_MODEL_OP_READING_SET_STATUS},
 };
 
 static esp_ble_mesh_client_t vendor_client = {
@@ -69,8 +77,8 @@ static esp_ble_mesh_client_t vendor_client = {
 };
 
 static esp_ble_mesh_model_op_t vnd_op[] = {
-    {ESP_BLE_MESH_VND_MODEL_OP_SET, 0, 0},
-    {ESP_BLE_MESH_VND_MODEL_OP_SET_STATUS, 0, 0},
+    {ESP_BLE_MESH_VND_MODEL_OP_READING_SET, 0, 0},
+    {ESP_BLE_MESH_VND_MODEL_OP_READING_SET_STATUS, 0, 0},
     ESP_BLE_MESH_MODEL_OP_END,
 };
 
@@ -106,8 +114,7 @@ static void prov_complete(uint16_t net_idx, uint16_t addr, uint8_t flags, uint32
 static void example_ble_mesh_provisioning_cb(esp_ble_mesh_prov_cb_event_t event,
                                              esp_ble_mesh_prov_cb_param_t *param)
 {
-    switch (event)
-    {
+    switch (event) {
     case ESP_BLE_MESH_PROV_REGISTER_COMP_EVT:
         ESP_LOGI(TAG, "ESP_BLE_MESH_PROV_REGISTER_COMP_EVT, err_code %d", param->prov_register_comp.err_code);
         break;
@@ -116,18 +123,19 @@ static void example_ble_mesh_provisioning_cb(esp_ble_mesh_prov_cb_event_t event,
         break;
     case ESP_BLE_MESH_NODE_PROV_LINK_OPEN_EVT:
         ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_LINK_OPEN_EVT, bearer %s",
-                 param->node_prov_link_open.bearer == ESP_BLE_MESH_PROV_ADV ? "PB-ADV" : "PB-GATT");
+            param->node_prov_link_open.bearer == ESP_BLE_MESH_PROV_ADV ? "PB-ADV" : "PB-GATT");
         break;
     case ESP_BLE_MESH_NODE_PROV_LINK_CLOSE_EVT:
         ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_LINK_CLOSE_EVT, bearer %s",
-                 param->node_prov_link_close.bearer == ESP_BLE_MESH_PROV_ADV ? "PB-ADV" : "PB-GATT");
+            param->node_prov_link_close.bearer == ESP_BLE_MESH_PROV_ADV ? "PB-ADV" : "PB-GATT");
         break;
     case ESP_BLE_MESH_NODE_PROV_COMPLETE_EVT:
         ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_COMPLETE_EVT");
         prov_complete(param->node_prov_complete.net_idx, param->node_prov_complete.addr,
-                      param->node_prov_complete.flags, param->node_prov_complete.iv_index);
+            param->node_prov_complete.flags, param->node_prov_complete.iv_index);
         break;
     case ESP_BLE_MESH_NODE_PROV_RESET_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_RESET_EVT");
         break;
     case ESP_BLE_MESH_NODE_SET_UNPROV_DEV_NAME_COMP_EVT:
         ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_SET_UNPROV_DEV_NAME_COMP_EVT, err_code %d", param->node_set_unprov_dev_name_comp.err_code);
@@ -135,6 +143,135 @@ static void example_ble_mesh_provisioning_cb(esp_ble_mesh_prov_cb_event_t event,
     default:
         break;
     }
+}
+
+int bt_mesh_device_auto_enter_network(struct bt_mesh_device_network_info *info)
+{
+    BT_INFO("bt_mesh_device_auto_enter_network()");
+
+    const struct bt_mesh_comp *comp = NULL;
+    struct bt_mesh_model *model = NULL;
+    struct bt_mesh_elem *elem = NULL;
+    struct bt_mesh_app_keys *keys = NULL;
+    struct bt_mesh_app_key *key = NULL;
+    struct bt_mesh_subnet *sub = NULL;
+    int i, j, k;
+    int err = 0;
+
+    BT_INFO("bt_mesh_atomic_set_bit()");
+
+    bt_mesh_atomic_set_bit(bt_mesh.flags, BLE_MESH_NODE);
+
+    BT_INFO("bt_mesh_provision()");
+
+    err = bt_mesh_provision(info->net_key, info->net_idx, info->flags, info->iv_index, info->unicast_addr, info->dev_key);
+
+    if (err)
+    {
+        BT_ERR("bt_mesh_provision() failed (err %d)", err);
+        return err;
+    }
+
+    sub = bt_mesh_subnet_get(info->net_idx);
+
+    if (!sub)
+    {
+        BT_ERR("Invalid NetKeyIndex 0x%04x", info->net_idx);
+        return -1;
+    }
+
+    for (i = 0; i < ARRAY_SIZE(bt_mesh.app_keys); i++)
+    {
+        key = &bt_mesh.app_keys[i];
+        if (key->net_idx == BLE_MESH_KEY_UNUSED)
+        {
+            break;
+        }
+    }
+    if (i == ARRAY_SIZE(bt_mesh.app_keys))
+    {
+        BT_ERR("Failed to allocate AppKey, 0x%04x", info->app_idx);
+        return -1;
+    }
+
+    keys = sub->kr_flag ? &key->keys[1] : &key->keys[0];
+
+    if (bt_mesh_app_id(info->app_key, &keys->id))
+    {
+        BT_ERR("Failed to calculate AID, 0x%04x", info->app_idx);
+        return -1;
+    }
+
+    key->net_idx = info->net_idx;
+    key->app_idx = info->app_idx;
+    memcpy(keys->val, info->app_key, 16);
+
+    /* Binds AppKey with all non-config models, adds group address to all these models */
+    comp = bt_mesh_comp_get();
+    if (!comp)
+    {
+        BT_ERR("Invalid composition data");
+        return -1;
+    }
+
+    for (i = 0; i < comp->elem_count; i++)
+    {
+        elem = &comp->elem[i];
+        for (j = 0; j < elem->model_count; j++)
+        {
+            model = &elem->models[j];
+            if (model->id == BLE_MESH_MODEL_ID_CFG_CLI)
+            {
+                continue;
+            }
+            for (k = 0; k < ARRAY_SIZE(model->keys); k++)
+            {
+                if (model->keys[k] == BLE_MESH_KEY_UNUSED)
+                {
+                    model->keys[k] = info->app_idx;
+                    break;
+                }
+            }
+            for (k = 0; k < ARRAY_SIZE(model->groups); k++)
+            {
+                if (model->groups[k] == BLE_MESH_ADDR_UNASSIGNED)
+                {
+                    model->groups[k] = info->group_addr;
+                    break;
+                }
+            }
+        }
+        for (j = 0; j < elem->vnd_model_count; j++)
+        {
+            model = &elem->vnd_models[j];
+            for (k = 0; k < ARRAY_SIZE(model->keys); k++)
+            {
+                if (model->keys[k] == BLE_MESH_KEY_UNUSED)
+                {
+                    model->keys[k] = info->app_idx;
+                    break;
+                }
+            }
+            for (k = 0; k < ARRAY_SIZE(model->groups); k++)
+            {
+                if (model->groups[k] == BLE_MESH_ADDR_UNASSIGNED)
+                {
+                    model->groups[k] = info->group_addr;
+                    break;
+                }
+            }
+        }
+    }
+
+    BT_INFO("exit bt_mesh_device_auto_enter_network()");
+
+    return 0;
+}
+
+static void example_ble_mesh_custom_model_cb(esp_ble_mesh_model_cb_event_t event,
+                                             esp_ble_mesh_model_cb_param_t *param)
+{
+
 }
 
 static esp_err_t ble_mesh_init(void)
@@ -182,7 +319,7 @@ static esp_err_t ble_mesh_init(void)
         .net_idx = net_idx,
         .flags = flags,
         .iv_index = iv_index,
-        .unicast_addr = 0x0063,
+        .unicast_addr = 0x0066,
         .dev_key = {
             0x01,
             0x23,
@@ -229,6 +366,10 @@ static esp_err_t ble_mesh_init(void)
     return err;
 }
 
+#define MSG_SEND_TTL        3
+#define MSG_TIMEOUT         0
+#define MSG_ROLE            ROLE_PROVISIONER
+
 void app_main(void)
 {
     esp_err_t err;
@@ -266,4 +407,19 @@ void app_main(void)
     }
 
     ESP_LOGI(TAG, "BLE mesh init complete");
+
+    esp_ble_mesh_msg_ctx_t ctx = {0};
+    uint32_t opcode;
+
+    ctx.net_idx = 0;
+    ctx.app_idx = 0;
+    ctx.addr = 0x0063;
+    ctx.send_ttl = 7;
+    ctx.send_rel = true;
+    opcode = ESP_BLE_MESH_VND_MODEL_OP_READING_SET;
+
+    err = esp_ble_mesh_client_model_send_msg(vendor_client.model, &ctx, opcode, 8, 0x00, MSG_TIMEOUT, true, ROLE_NODE);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to send vendor message 0x%06" PRIx32, opcode);
+    }
 }
